4d47f57b455cc040191ce77ab03fcb77c5a097f7
ManhblueCW <homanhkhongdau@gmail.com>
2023-12-10 21:30:15 +0700

    first init

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+}
---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import javafx.scene.control.TextField;
+
+public class Message extends TextField {
+    public Message(String text) {
+        setText(text);
+    }
+
+}
